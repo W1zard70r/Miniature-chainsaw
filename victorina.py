cea52b2686769c08eb4247813d41da88dd97add2
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2022-12-03 09:07:51 +0700

    Add files via upload

--- victorina.py
+++ victorina.py
@@ -1,10 +1,25 @@
 a = ["какой сейчас год?", "самый старый учитель ЛТПУ?", 'Веришь в бога?',
-     'кому слуэжили миньоны в 1945?', 'Сколько углов в треугольнике?', "сколько часов спит лицеист?"]
+     'айтишник ли ты?', 'Сколько углов в треугольнике?']
 scores = int(0)
 for i in range(len(a)):
     if i == 0:
         answer = int(input(f'{a[i]}\n'))
         if answer == 2022:
             scores += 1
-print(scores)
-    202
+    if i == 1:
+        answer = int(input(f'{a[i]}\n   1 - Лариса Хазиевна Казанцева\n   2 - Ольга Владимировна\n   3 - Семен Даньшин\n   4 - Надежда Усова\n'))
+        if answer == 1:
+            scores += 1
+    if i == 2:
+        answer = input(f'{a[i]}\n')
+        if answer == 'да':
+            scores += 1
+    if i == 3:
+        answer = input(f'{a[i]}\n')
+        if answer == 'да':
+            scores += 1
+    if i ==4:
+        answer = int(input(f'{a[i]}\n'))
+        if answer == 3:
+            scores += 1
+print(f'оценка тебя - {scores}')
