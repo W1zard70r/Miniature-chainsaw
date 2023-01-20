6341a02e460e2b15137f24e9baf7a12dba5b50e8
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2023-01-20 10:02:35 +0700

    Create 12(2).py

---
+++ 12(2).py
@@ -0,0 +1,13 @@
+nums=[2,3,5,7]
+for i in range(8,1000):
+    flag=True
+    for b in range(len(nums)):
+        if i%nums[b] == 0:
+            flag=False
+    if flag:
+        nums.append(i)
+print(nums)
+for n in range(30):
+    for i in range(len(nums)):
+        if 6*n + 105 ==nums[i]:
+            print(n)
