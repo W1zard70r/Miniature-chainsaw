2873093bdd87a0c25a47432115fc120124c897e1
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2023-02-13 13:25:31 +0700

    Update конструкции.py

--- 2 Полугодие/конструкции.py
+++ 2 Полугодие/конструкции.py
@@ -1,4 +1,16 @@
+#map
 a=[1,2,3,'0']
 b=map(str,a)
 c=list(map(int,b))
 print(c)
+
+#lambda-функция
+a=[1,2,3,4,5,7]
+c=list(map(lambda x: x*2,a))
+print(c)
+
+a=[1,2,3,4,5,7]
+b=['193','8','2','3','2','1']
+c=list(map(lambda x: int(x) if int(x)>2 else 0,b))
+f=[int(x) for x in b if int(x)>2]
+print(c,f)
