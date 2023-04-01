89bcb56514aadfce859c0204e9e849284a5bad0d
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2023-04-01 13:54:14 +0700

    Update spisoksort.py

--- 2 Полугодие/spisoksort.py
+++ 2 Полугодие/spisoksort.py
@@ -1,16 +1,14 @@
-a=[int(x) for x in input().split()]
+a=[int(x) for x in input().split()]  # ввод списка
 e=len(a)
 Fl=False
-while not(Fl):
-    for i in range(e):
+while not(Fl):      # бесконечный цикл для сортировки списка с условием выхода
+    for i in range(e):          # сравнение элемента со всеми элементами списка и замена
         f=a[i]
         for x in range(i,e):
             if f<=a[x]:
                 continue
             else:
                 a[i],a[x]=a[x],a[i]
-        
-    print(a)
-    if all(a[i]<=a[i+1] for i in range(e-1)):
+    if all(a[i]<=a[i+1] for i in range(e-1)):   # проверка на верную сортировку
         Fl=True
-print('end - ',a)
+print('end - ',a)   
