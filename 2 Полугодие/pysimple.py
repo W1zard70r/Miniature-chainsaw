2898b2afdd622a9193d53ddfb81483038ad2f2f0
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2023-04-14 21:00:46 +0700

    Update pysimple.py

--- 2 Полугодие/pysimple.py
+++ 2 Полугодие/pysimple.py
@@ -1,4 +1,4 @@
-import PySimpleGUI as sg
+\import PySimpleGUI as sg
 
 
 sg.theme('LightBlue2')  # Set the theme
@@ -18,14 +18,14 @@ for z in range():
 # Define the layout
 layout = [[sg.Text('Input Text:', font=('Arial', 12)), sg.InputText(key='input')],
          [sg.Text('Input number of text:', font=('Arial', 12)), sg.InputText(key='inputt')],
-          [sg.Text('Output Text:', font=('Arial', 12)), sg.Text('', size=(40, 1), key='output')],
+          [sg.Text('added:', font=('Arial', 12)), sg.Text('', size=(40, 1), key='output')],
           [sg.Combo(s, default_value=s[0], s=(15,22), enable_events=True, readonly=True, k='-COMBO-', key='Combo'),
           sg.Output(s=(40,10), key='outputt')],
           [sg.Button('Process Input', font=('Arial', 12), button_color=('white', '#4CAF50'), key='process'),
-           sg.Button('Someth', font=('Arial', 12), button_color=('white', '#4CAF50'), key='button')]]
+           sg.Button('Someth', font=('Arial', 12), button_color=('white', '#4CAF50'), key='button'),sg.Button('to add', font=('Arial', 12), button_color=('white', '#4CAD50'), key='buttonn')]]
 
 # Create the window
-window = sg.Window('ЕГЭ архив', layout)
+window = sg.Window('ЕГЭ by Alexandr Silkov', layout)
 
 # Event loop
 while True:
@@ -36,12 +36,20 @@ while True:
         break
 
     # Process the input and update the output when the button is clicked
+    if event == 'buttonn':
+        input_text = values['input']
+        input_num= values['inputt']
+        a.append(input_text)
+        s.append(str(input_num))
+        window['output'].update('')
     if event == 'process':
         input_text = values['input']
-        input_num=
+        input_num= values['inputt']
+        window['outputt'].update('')
         choice=a[int(values['Combo'])-1]
-        window['input'].update('яяя')
+        window['input'].update('')
         window['output'].update(input_text)
-        window['outputt'].update(choice)
+        print(s)
+        print(a)
     if event == 'button':
         break
