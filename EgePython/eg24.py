8cb27683fdc30c167b9a8cfa141eea9687c57441
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2023-05-20 14:10:27 +0700

    Add files via upload

--- EgePython/eg24.py
+++ EgePython/eg24.py
@@ -1,18 +1,15 @@
 f=open('24-241.txt','r')
-f=f.readline()
+f=f.readline().split('E')
 coun=[]
-while f.count('E')>1:
-    l=f.index('E')
-    f=f.replace('E','*',1)
-    p=f.index('E')
-    f=f.replace('E','*',1)
-    a=f[l+1:p]
-    if a.count('B')==2:
-        lb=a.index('B')
-        a.replace('B','*',1)
-        rb=a.index('B')
-        a.replace('B','*',1)
-        if a.count('A')>5:
-            coun.append(2+p-l)
-    f.replace('B','*',a.count('B'))
+for i in range(len(f)):
+    if f[i].count('A')>5 and f[i].count('B')==2:
+        t=0
+        y=0
+        for b in range(len(f[i])):
+            if f[i][b]=='B':
+                t+=1
+            if f[i][b]=='A' and t==1:
+                y+=1
+        if y>5:
+            coun.append(2+len(f[i]))
 print(min(coun))
